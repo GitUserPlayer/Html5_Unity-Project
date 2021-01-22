793e8ca062132d292d3aa3570bd5d572d03ff542
zheUser <hkkb8670@gmail.com>
2021-01-22 17:31:01 +0800

    建立html5環境

---
+++ Assets/LookAtScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtScript : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation((pos*Time.deltaTime)/speed);
+        /*
+        Vector3 relativePos = (target.position + new Vector3(0, 1.5f, 0)) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+        transform.Translate(0, 0, 3 * Time.deltaTime*speed);*/
+       
+
+    }
+}
---
+++ Assets/testMo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testMo : MonoBehaviour
+{
+    public float speed = 6f;
+    
+
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+        //Input.GetAxis("Horizontal") *
+    }
+}
